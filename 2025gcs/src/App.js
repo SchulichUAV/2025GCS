7545743676b44d3dadcc48e4100cad6d8a44eaef
Shahed <shahedissa9@gmail.com>
2024-10-26 20:19:27 -0600

    fit components to page

--- 2025gcs/src/App.js
+++ 2025gcs/src/App.js
@@ -9,22 +9,22 @@ import AltitudePanel from './Components/AltitudePanel/AltitudePanel';
 
 function App() {
   return (
-    <div className="flex flex-col items-center min-h-screen bg-gray-100">
+    <div className="flex flex-col h-screen w-screen bg-gray-100">
       <NavBar />
-      <div className="grid grid-cols-2 gap-4 mt-20 p-4 w-full max-w-5xl">
-        <div className="border border-gray-300 p-4 text-center col-span-1 row-span-2">
+      <div className="grid grid-cols-2 gap-4 p-4 h-full w-full mt-16"> {/* Adjusting spacing from NavBar */}
+        <div className="border border-gray-300 p-4 flex justify-center items-center" style={{ gridColumn: "1 / 2", gridRow: "1 / span 2", height: '80%' }}>
           <PhotoPanel />
         </div>
-        <div className="border border-gray-300 p-4 text-center">
+        <div className="border border-gray-300 p-4 flex justify-center items-center" style={{ gridColumn: "2 / 3", height: '40%' }}>
           <PayloadPanel />
         </div>
-        <div className="border border-gray-300 p-4 text-center">
+        <div className="border border-gray-300 p-4 flex justify-center items-center" style={{ gridColumn: "2 / 3", height: '40%' }}>
           <AIPanel />
         </div>
-        <div className="border border-gray-300 p-4 text-center">
+        <div className="border border-gray-300 p-4 flex justify-center items-center" style={{ gridColumn: "1 / 2", gridRow: "3 / 4", height: '20%' }}>
           <FlightModePanel />
         </div>
-        <div className="border border-gray-300 p-4 text-center">
+        <div className="border border-gray-300 p-4 flex justify-center items-center" style={{ gridColumn: "2 / 3", gridRow: "3 / 4", height: '20%' }}>
           <AltitudePanel />
         </div>
       </div>
