1486dce7d7fe6e022da1b95c2175021d8e6c376d
itsrabikhan <26781039+itsrabikhan@users.noreply.github.com>
2024-11-02 13:24:53 -0600

    Progress on AltitudePanel. - Almost everything is complete, except for some fixes with flexboxes regarding the "ft" text in the input boxes and the overall panel size. - Foundation for POST requests has been setup, is functional but will not work without backend (obviously).

--- 2025gcs/src/Components/NavBar/NavBar.js
+++ 2025gcs/src/Components/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = () => (
   <nav className="w-full flex items-center justify-between px-8 py-4 bg-gray-800 text-white fixed top-0">
     <div className="space-x-5 flex items-center">
-      <img src="./SUAVIcon.jpg" alt="Logo" className="h-8 w-8" /> {/* need transparent logo, ask on saturday */}
+      <img src="SUAVIcon.jpg" alt="Logo" className="h-12 w-12 px-1 py-1" /> {/* need transparent logo, ask on saturday */}
       <label className="font-bold">Ground Control Station</label>
     </div>
     <div className="space-x-4">
