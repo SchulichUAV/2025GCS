1389f5125a70f4f0816f1b4a3acd4c8861481d59
itsrabikhan <26781039+itsrabikhan@users.noreply.github.com>
2024-11-02 12:05:14 -0600

    Started work on AltitudePanel. - NavBar temporarily has a non-transparent logo.   - There are two files due to issues with pathing, will fix soon. - AltitudePanel is very basic and does not contain background or proper padding, will fix later as well.

--- 2025gcs/src/Components/AltitudePanel/AltitudePanel.js
+++ 2025gcs/src/Components/AltitudePanel/AltitudePanel.js
@@ -3,8 +3,15 @@ import React from 'react'
 const AltitudePanel = () => {
   return (
     <div className="altitude-panel">
-    AltitudePanel
-  </div>
+      <div className="bg-gray-200 p-2 mb-2">
+        <input type="text" placeholder="Takeoff Altitude" className="border border-gray-300 rounded-md p-2" style={{ background: "white" }} />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2">Submit</button>
+      </div>
+      <div className="bg-gray-200 p-2">
+        <input type="text" placeholder="Go-to Altitude" className="border border-gray-300 rounded-md p-2" style={{ background: "white" }} />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-md ml-2">Submit</button>
+      </div>
+    </div>
   )
 }
 
--- 2025gcs/src/Components/NavBar/NavBar.js
+++ 2025gcs/src/Components/NavBar/NavBar.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const NavBar = () => (
   <nav className="w-full flex items-center justify-between px-8 py-4 bg-gray-800 text-white fixed top-0">
-    <div className="text-xl font-bold">🚀</div>
+    <div className="space-x-5 flex items-center">
+      <img src="./SUAVIcon.jpg" alt="Logo" className="h-8 w-8" /> {/* need transparent logo, ask on saturday */}
+      <label className="font-bold">Ground Control Station</label>
+    </div>
     <div className="space-x-4">
       <a href="#home" className="hover:underline">Home</a>
       <a href="#data" className="hover:underline">Data</a>
