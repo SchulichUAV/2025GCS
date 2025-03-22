863eb259bbfb4389d2d21bf5045ee098f8022a7a
Steven Huang <138158022+stevenhuang007@users.noreply.github.com>
2025-03-22 14:26:15 -0600

    Update to plane flight modes.

--- 2025gcs/frontend/src/Components/Home/FlightMode/FlightModePanel.js
+++ 2025gcs/frontend/src/Components/Home/FlightMode/FlightModePanel.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { DRONE_IP } from '../../../config';
-import ReactSlider from 'react-slider';
+import React, { useState } from "react";
+import { DRONE_IP } from "../../../config";
+import ReactSlider from "react-slider";
 
 const FlightControl = () => {
   const [isUnlocked, setIsUnlocked] = useState(false);
   const [sliderValue, setSliderValue] = useState(0);
   const [lockIcon, setLockIcon] = useState(
-    'https://as1.ftcdn.net/v2/jpg/09/71/35/30/1000_F_971353035_aVx5TB2fKRp9pd6EKtuGFN6CalQekcQ3.jpg'
+    "https://as1.ftcdn.net/v2/jpg/09/71/35/30/1000_F_971353035_aVx5TB2fKRp9pd6EKtuGFN6CalQekcQ3.jpg"
   );
 
   const handleSetFlightMode = async (mode_id) => {
@@ -14,9 +14,9 @@ const FlightControl = () => {
     relockSlider();
     const data = { mode_id };
     await fetch(`http://${DRONE_IP}:5000/set_flight_mode`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
@@ -27,9 +27,9 @@ const FlightControl = () => {
     relockSlider();
     const data = { altitude };
     await fetch(`http://${DRONE_IP}:5000/takeoff`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
@@ -37,9 +37,7 @@ const FlightControl = () => {
 
   const unlockSlider = () => {
     setIsUnlocked(true);
-    setLockIcon(
-      'https://www.svgrepo.com/show/326700/lock-open-outline.svg'
-    );
+    setLockIcon("https://www.svgrepo.com/show/326700/lock-open-outline.svg");
     setTimeout(() => relockSlider(), 10000);
   };
 
@@ -47,7 +45,7 @@ const FlightControl = () => {
     setIsUnlocked(false);
     setSliderValue(0);
     setLockIcon(
-      'https://as1.ftcdn.net/v2/jpg/09/71/35/30/1000_F_971353035_aVx5TB2fKRp9pd6EKtuGFN6CalQekcQ3.jpg'
+      "https://as1.ftcdn.net/v2/jpg/09/71/35/30/1000_F_971353035_aVx5TB2fKRp9pd6EKtuGFN6CalQekcQ3.jpg"
     );
   };
 
@@ -68,7 +66,12 @@ const FlightControl = () => {
   return (
     <div className="py-4 px-20 w-full mx-auto space-y-2 bg-white rounded-xl shadow-lg">
       <div className="flex items-center space-x-4">
-        <img src={lockIcon} alt="Lock Icon" className="w-8 h-8" draggable="false"/>
+        <img
+          src={lockIcon}
+          alt="Lock Icon"
+          className="w-8 h-8"
+          draggable="false"
+        />
         <ReactSlider
           className="w-full h-2 bg-gray-300 rounded-lg cursor-pointer"
           thumbClassName="transform -translate-y-1 w-4 h-4 bg-blue-500 rounded-full cursor-pointer focus:outline-none"
@@ -80,19 +83,21 @@ const FlightControl = () => {
           onAfterChange={handleSliderAfterChange}
         />
         <span className="text-gray-500 select-none">
-          {sliderValue === 100 ? 'Unlocked!' : 'Locked'}
+          {sliderValue === 100 ? "Unlocked!" : "Locked"}
         </span>
       </div>
       <div className="grid grid-cols-2 gap-3">
         {[
-          { label: 'Loiter', id: 5 },
-          { label: 'Stabilize', id: 0 },
-          { label: 'Guided', id: 4 },
-          { label: 'RTL', id: 6 },
-          { label: 'Auto', id: 3 },
-          { label: 'Land', id: 9 },
-          { label: 'Alt Hold', id: 2 },
-          { label: 'Takeoff', id: 'takeoff', isTakeoff: true },
+          { label: "Manual", id: 0 },
+          { label: "Circle", id: 1 },
+          { label: "Stablilize", id: 2 },
+          { label: "Fly_By_Wire_A", id: 5 },
+          { label: "Fly_By_Wire_B", id: 6 },
+          { label: "Cruise", id: 7 },
+          { label: "Auto", id: 10 },
+          { label: "RTL", id: 11 },
+          { label: "Loiter", id: 12 },
+          { label: "Guided", id: 15 },
         ].map((mode) => (
           <button
             key={mode.label}
@@ -101,7 +106,7 @@ const FlightControl = () => {
             }
             disabled={!isUnlocked}
             className={`px-4 py-2 bg-gray-500 hover:bg-gray-400 text-white rounded-md focus:outline-none focus:ring-4 ${
-              isUnlocked ? 'cursor-pointer' : 'cursor-not-allowed opacity-50'
+              isUnlocked ? "cursor-pointer" : "cursor-not-allowed opacity-50"
             }`}
           >
             {mode.label}
